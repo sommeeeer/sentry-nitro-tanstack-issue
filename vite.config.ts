c2df901854dcc797ca40f7da0ff51d725caee469
magnus <magnus@dahleide.com>
2026-02-26 21:37:40 +0100

    add sentry cli

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,28 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
 import { sentryTanstackStart } from "@sentry/tanstackstart-react";
+import SentryCli from '@sentry/cli';
+import { readdir, rm } from "node:fs/promises";
+import { join, resolve } from "node:path";
+
+async function deleteSourceMaps(dir: string) {
+  const entries = await readdir(dir, { withFileTypes: true });
+
+  await Promise.all(
+    entries.map(async (entry) => {
+      const entryPath = join(dir, entry.name);
+
+      if (entry.isDirectory()) {
+        await deleteSourceMaps(entryPath);
+        return;
+      }
+
+      if (entry.isFile() && entry.name.endsWith(".map")) {
+        await rm(entryPath, { force: true });
+      }
+    }),
+  );
+}
 
 const config = defineConfig({
   build: {
@@ -25,22 +47,41 @@ const config = defineConfig({
       org: "testappliaction",
       project: "javascript-tanstackstart-react",
       authToken: process.env.SENTRY_AUTH_TOKEN,
+      // sourcemaps: {
+      //   // SST uses the `.output/public` folder as static asssets:
+      //   // https://github.com/sst/sst/blob/432418a2bb8/platform/src/components/aws/tan-stack-start.ts#L409
+      //   assets: ["./.output/**/*"],
+      //   ignore: ["**/node_modules/**"],
+      //   filesToDeleteAfterUpload: ["./.output/**/*.map"],
+      // },
+      // debug: true,
       sourcemaps: {
-        // SST uses the `.output/public` folder as static asssets:
-        // https://github.com/sst/sst/blob/432418a2bb8/platform/src/components/aws/tan-stack-start.ts#L409
-        assets: ["./.output/**/*"],
-        ignore: ["**/node_modules/**"],
-        filesToDeleteAfterUpload: ["./.output/**/*.map"],
-      },
-      debug: true,
+        disable: true,
+      }
     }),
   ],
-  nitro: {
-    sourcemap: true,
-    preset: "aws-lambda",
-    awsLambda: {
-      streaming: true,
-    },
+nitro: {
+  sourcemap: true,
+  preset: "aws-lambda",
+  awsLambda: {
+    streaming: true,
+  },
+  hooks: {
+    compiled: async () => {
+      const outputDir = resolve(process.cwd(), ".output/public/");
+
+      const sentryCli = new SentryCli(null, {
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "testappliaction",
+        project: "javascript-tanstackstart-react",
+      });
+
+      await sentryCli.execute(["sourcemaps", "inject", outputDir], true);
+      await sentryCli.execute(["sourcemaps", "upload", outputDir], true);
+
+      await deleteSourceMaps(outputDir);
+    }
+  }
   },
 });
 
